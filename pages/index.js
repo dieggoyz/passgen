11ec3d0cf588e897ef883554b90fe6bfed17fde0
diegoeffar <diegoeffar@gmail.com>
2024-04-10 18:55:25 -0400

    title added

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import Head from 'next/head'
 import { Inter } from 'next/font/google'
 import { Header, Generator, Footer } from '@/components'
 
@@ -5,10 +6,17 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function Home () {
   return (
-    <main className={`flex min-h-screen flex-col items-center justify-between gap-4 ${inter.className} p-4`}>
-      <Header />
-      <Generator />
-      <Footer />
-    </main>
+    <>
+      <Head>
+        <meta name="description" content="Generate secure passwords with a click of a button." />
+        <title>Password Generator — dieggoyz</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className={`flex min-h-screen flex-col items-center justify-between gap-4 ${inter.className} p-4`}>
+        <Header />
+        <Generator />
+        <Footer />
+      </main>
+    </>
   )
 }
