027e91b879ff40fd6738bb88b935c4a46932b391
diegoeffar <diegoeffar@gmail.com>
2024-04-11 00:45:37 -0400

    Inter font added as variable

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import { Inter } from 'next/font/google'
 import { Header, Generator, Footer } from '@/components'
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'], variable: '--font-sans' })
 
 export default function Home () {
   return (
@@ -12,7 +12,7 @@ export default function Home () {
         <title>Password Generator — dieggoyz</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={`flex min-h-screen flex-col items-center justify-between gap-4 ${inter.className} p-4`}>
+      <main className={`bg-white dark:bg-neutral-950 text-black dark:text-neutral-200 flex min-h-screen flex-col items-center justify-between gap-4 text-md ${inter.variable} font-sans`}>
         <Header />
         <Generator />
         <Footer />
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,11 @@ module.exports = {
     './app/**/*.{js,ts,jsx,tsx,mdx}'
   ],
   theme: {
-    extend: {}
+    extend: {
+      fontFamily: {
+        sans: ['var(--font-sans)']
+      }
+    }
   },
   plugins: [
     require('@tailwindcss/forms')
