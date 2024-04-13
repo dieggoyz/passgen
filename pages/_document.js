977b1751d68a17cf57ff83b90acf807f2e31017e
diegoeffar <diegoeffar@gmail.com>
2024-04-13 04:23:36 -0400

    Favicon added and title updated

--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,13 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document"
 
-export default function Document () {
-  return (
-    <Html lang="en">
-      <Head />
-      <body className='bg-zinc-950 text-zinc-300 antialiased'>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+export default function Document() {
+	return (
+		<Html lang="en">
+			<Head />
+			<body className="bg-zinc-950 text-zinc-300 antialiased">
+				<Main />
+				<NextScript />
+			</body>
+		</Html>
+	)
 }
--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,27 @@
-import Head from 'next/head'
-import { Inter } from 'next/font/google'
-import { Header, Generator, Footer } from '@/components'
+import Head from "next/head"
+import { Inter } from "next/font/google"
+import { Header, Generator, Footer } from "@/components"
 
-const inter = Inter({ subsets: ['latin'], variable: '--font-sans' })
+const inter = Inter({ subsets: ["latin"], variable: "--font-sans" })
 
-export default function Home () {
-  return (
-    <>
-      <Head>
-        <meta name="description" content="Generate secure passwords with a click of a button." />
-        <title>Password Generator — dieggoyz</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <main className={`flex flex-col h-screen items-center justify-between gap-4 text-md ${inter.variable} font-sans`}>
-        <Header />
-        <Generator />
-        <Footer />
-      </main>
-    </>
-  )
+export default function Home() {
+	return (
+		<>
+			<Head>
+				<meta
+					name="description"
+					content="Generate secure passwords with a click of a button."
+				/>
+				<title>Password Generator — @dieggoyz</title>
+				<link rel="icon" type="image/png" href="/favicon.png" />
+			</Head>
+			<main
+				className={`text-md flex h-screen flex-col items-center justify-between gap-4 ${inter.variable} font-sans`}
+			>
+				<Header />
+				<Generator />
+				<Footer />
+			</main>
+		</>
+	)
 }
