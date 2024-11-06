574189e3607aeb2515414c8bc3dee28eb715179d
dieggoyz <diegoeffar@gmail.com>
2024-11-06 04:01:49 -0300

    📦 chore: minor tweak

---
+++ components/signature.jsx
@@ -0,0 +1,27 @@
+import React from "react"
+
+export default function Signature() {
+	return (
+		<div
+			style={{
+				bottom: "1rem",
+				position: "fixed",
+				right: "1rem",
+			}}
+		>
+			<a
+				href="https://github.com/dieggoyz"
+				rel="noopener noreferrer"
+				target="_blank"
+				style={{
+					color: "#8a93a0",
+					fontFamily: "monospace",
+					fontSize: ".875rem",
+					textDecoration: "none",
+				}}
+			>
+				〆 dieggoyz
+			</a>
+		</div>
+	)
+}
